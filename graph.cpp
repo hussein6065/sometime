b51170a82ac11cfe16163c613278253745b34d01
Hussein Baba Fuseini <43960479+hussein6065@users.noreply.github.com>
2020-05-17 13:43:03 +0000

    Update graph.cpp

--- graph.cpp
+++ graph.cpp
@@ -15,13 +15,37 @@ class Person{
     double d_prob;
     int howlong;
     int covidStatus;
-    vector<Person*> connectedPeople;
+    vector<Person*> *connectedPeople;
 
     public:
         Person(string name, int age, double s_prob){
             this -> name = name;
             this -> age = age;
             this -> s_prob = s_prob;
+            this -> covidStatus = 0;
+            this->howlong = 0;
+            this -> connectedPeople = new vector<Person*>();
+
+        }
+        bool checkIn(Person* node){
+            for(auto person: *connectedPeople){
+                if(node == person){
+                    return true;
+                }
+            }
+            return false;
+        }
+        
+        void insertNode(Person* node){
+            if(!checkIn(node)){
+                connectedPeople->push_back(node);
+            }
         }
 
+        // You can add the gettors and settor for the instance variables
+
 };
+
+int main(){
+    
+}
