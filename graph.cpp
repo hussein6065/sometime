b9d881fe360dc57383de7294bcd37cdcd8df2cf0
Hussein Baba Fuseini <43960479+hussein6065@users.noreply.github.com>
2020-05-17 13:06:55 +0000

    Update graph.cpp

--- graph.cpp
+++ graph.cpp
@@ -1,1 +1,27 @@
+#include<iostream>
+#include<string>
+#include<fstream>
+#include<vector>
+#include<sstream>
+#include<time.h>
 
+using namespace std;
+
+class Person{
+    
+    string name;
+    int age;
+    double s_prob;
+    double d_prob;
+    int howlong;
+    int covidStatus;
+    vector<Person*> connectedPeople;
+
+    public:
+        Person(string name, int age, double s_prob){
+            this -> name = name;
+            this -> age = age;
+            this -> s_prob = s_prob;
+        }
+
+};
